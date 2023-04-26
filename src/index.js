8eb7e364d25829fbeba6264cec23b94e004feacc
Denis <ggmazanovdenis@gmail.com>
2023-04-26 20:25:50 +0300

    creating an application template

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 );
 
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+);
