396f8056de9f1d4871d8821a14d34d5c29c31494
Denis <ggmazanovdenis@gmail.com>
2023-04-28 16:49:24 +0300

    mastering state lifting

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -19,6 +19,7 @@ class EmployeesAddForm extends Component {
 
     onSubmit = (e) => {
         e.preventDefault();
+        if(this.state.name.length < 3 || !this.state.salary) return;
         this.props.onAdd(this.state.name, this.state.salary);
         this.setState({
             name: '',
@@ -33,7 +34,8 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form 
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onClick={this.onSubmit}>
                     <input type="text" 
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
@@ -48,8 +50,7 @@ class EmployeesAddForm extends Component {
                         onChange={this.onValueChange}/>
                     
                     <button type="submit"
-                            className="btn btn-outline-light"
-                            onClick={this.onSubmit}>Добавить</button>
+                            className="btn btn-outline-light">Добавить</button>
                 </form>
             </div>
         )
